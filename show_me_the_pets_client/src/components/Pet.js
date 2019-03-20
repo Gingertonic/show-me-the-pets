5747f452b50e359416359a38d03703cc3e1788cc
Gingertonic <bethmschofield@gmail.com>
2019-03-20 21:06:30 +0000

    showing siberian kitties

--- show_me_the_pets_client/src/components/Pet.js
+++ show_me_the_pets_client/src/components/Pet.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
 
 export default class Pet extends Component {
+  state = {}
+
+  getName = () => {
+    return (this.props.pet.name !== "") ? `Meet ${this.props.pet.name}` : `I don't have a name yet!`
+  }
+
+  getColor = () => {
+    return (this.props.pet.color !== "Unknown") ? this.props.pet.color : null;
+  }
+
+  componentDidMount = () => {
+    this.getBreedCode()
+  }
+
+  getBreedCode = () => {
+    fetch('https://api.thecatapi.com/v1/breeds/search?q=sib', {
+      headers: {"x-api-key": "36ff3545-500e-4d64-bcf7-4003b02f18df"}
+    })
+      .then(resp => resp.json())
+      .then(breed => this.getImage(breed[0].id))
+  }
+
+  getImage = breedCode => {
+    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedCode}&limit=1&api_key=36ff3545-500e-4d64-bcf7-4003b02f18df`)
+      .then(resp => resp.json())
+      .then(results => this.setState({image: results[0].url}))
+  }
+
+  renderImage = () => {
+    return (this.state.image) ? <img className="pet-pic" src={this.state.image} alt="pet pic"/> : null;
+  }
+
   render = () => {
     return(
       <div id="show-pet">
-        <h1>{this.props.pet.name}</h1>
+        <h1>{this.getName()}</h1>
+        {this.renderImage()}
+        <p>{this.props.pet.gender}, {this.getColor()} {this.props.pet.breed}</p>
+        <p>{this.props.pet.address}</p>
         <div className="nav-link" onClick={() => this.props.switchView('search')}>Back to Results</div>
       </div>
     )
